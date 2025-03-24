18287090e6cd0a51e493c6afaac2c5dc129c81eb
unknown <tokisword@gmail.com>
2025-03-24 08:47:04 +0300

    API client
    Mise en place API client

--- routes/api.php
+++ routes/api.php
@@ -12,9 +12,82 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+/* ->withoutMiddleware(['auth:api']) */
+Route::post('/login', 'App\Api\v1\Controllers\Auth\AuthController@login');
 
 Route::group(['namespace' => 'App\Api\v1\Controllers'], function () {
     Route::group(['middleware' => 'auth:api'], function () {
         Route::get('users', ['uses' => 'UserController@index']);
+        Route::get('clients', ['uses' => 'ClientController@index']);
+
+    
+        /**
+         * Users
+         */
+        /* Route::group(['prefix' => 'users'], function () {
+            Route::get('/data', 'UsersController@anyData')->name('users.data');
+        }); */
+        
     });
+}); 
+
+
+
+    
+
+/* Route::post('/loginAPI', 'App\Http\Controllers\Auth\LoginController@loginAPI'); */
+
+/* Route::group(['namespace' => 'App\Api\v1\Controllers', 'middleware' => 'auth:api'], function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+    Route::get('/users', 'UserController@index');
 });
+
+Route::post('/loginAPI', 'Api\ApiAuthController@login');
+// Dans routes/api.php
+Route::post('/login', 'Auth\LoginController@loginAPI')->withoutMiddleware(['web']); */
+
+/* -------------------------------
+    Routes Publiques (Sans Auth)
+---------------------------------- */
+/* Route::group(['namespace' => 'App\Http\Controllers'], function () {
+    Route::post('/login', 'Api\AuthController@login');
+}); 
+ */
+/* -------------------------------
+    Routes Protégées (Avec Token)
+---------------------------------- */
+/* Route::group([
+    'namespace' => 'App\Api\v1\Controllers',
+    'middleware' => ['auth:api']
+], function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+    Route::get('/users', 'UserController@index');
+}); */
+
+
+
+/* ---------------------------
+    Authentification API
+--------------------------- */
+/*  Route::post('/login', 'App\Http\Controllers\Api\AuthController@login');*/
+
+// routes/api.php
+
+
+/* ---------------------------
+    Routes Protégées
+--------------------------- */
+/* Route::group([
+    'middleware' => ['auth:api'], 
+    'namespace' => 'App\Api\v1\Controllers'  
+], function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+    
+    Route::get('/users', 'UserController@index');
+}); */
